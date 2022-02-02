97241b916e218e0a1d0b75e693102479801f71fc
DevExpressExampleBot <fakeEmail>
2022-02-02 11:52:10 +0300

    Source auto update [skip ci]

--- VB/Program.vb
+++ VB/Program.vb
@@ -1,27 +1,24 @@
-﻿Imports DevExpress.Pdf
+Imports DevExpress.Pdf
 
 Namespace GetRadioGroupCheckedValues
-	Friend Class Program
 
-		Shared Sub Main(ByVal args() As String)
-			' Load a document with an interactive form.
-			Dim processor As New PdfDocumentProcessor()
-			processor.LoadDocument("DocumentToFill.pdf")
+    Friend Class Program
 
-			' Retrieve the form field facade:
-			Dim documentFacade As PdfDocumentFacade = processor.DocumentFacade
-			Dim acroFormFacade As PdfAcroFormFacade = documentFacade.AcroForm
+        Shared Sub Main(ByVal args As String())
+            ' Load a document with an interactive form.
+            Dim processor As PdfDocumentProcessor = New PdfDocumentProcessor()
+            processor.LoadDocument("DocumentToFill.pdf")
+            ' Retrieve the form field facade:
+            Dim documentFacade As PdfDocumentFacade = processor.DocumentFacade
+            Dim acroFormFacade As PdfAcroFormFacade = documentFacade.AcroForm
+            ' Specify a checked appearance name for the Female radio button:
+            Dim genderField As PdfRadioGroupFormFieldFacade = acroFormFacade.GetRadioGroupFormField("Gender")
+            For Each item As PdfFormFieldItem In genderField.Field.Items
+                If Equals(item.Value, "Female") Then genderField.Value = item.Value
+            Next
 
-			' Specify a checked appearance name for the Female radio button:
-			Dim genderField As PdfRadioGroupFormFieldFacade = acroFormFacade.GetRadioGroupFormField("Gender")
-			For Each item As PdfFormFieldItem In genderField.Field.Items
-				If item.Value = "Female" Then
-					genderField.Value = item.Value
-				End If
-			Next item
-
-			' Save the modified document.
-			processor.SaveDocument("..\..\Result.pdf")
-		End Sub
-	End Class
+            ' Save the modified document.
+            processor.SaveDocument("..\..\Result.pdf")
+        End Sub
+    End Class
 End Namespace
